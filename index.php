f2fafa834c53b8bfea1bff0b36d53e986a2a0e5c
david-berruezo-gmail <davidberruezo33@gmail.com>
2024-05-17 11:11:36 +0200

    docker composer + dockerfile + config

---
+++ index.php
@@ -0,0 +1,310 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Design Patterns</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="./css/style.css" type="text/css">
+</head>
+<body>
+<div class="container">
+    <h1>Design Patterns</h1>
+    <div class="table">
+        <div class="table-header">
+            <h3>Image</h3>
+            <h3>Design Pattern</h3>
+            <h3>Conceptual</h3>
+            <h3>Real World</h3>
+            <h3>UML</h3>
+            <h3>Tests</h3>
+        </div>
+        <br>
+        <div id="table-body">
+            <div class="table-body">
+                <img src="./Adapter/uml/uml.png" alt="Adapter">
+                <h4>Adapter</h4>
+                <h4><a href="./Adapter/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Adapter/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Adapter/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Bridge/uml/uml.png" alt="Adapter">
+                <h4>Bridge</h4>
+                <h4><a href="./Bridge/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Bridge/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Bridge/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Builder/uml/uml.png" alt="Adapter">
+                <h4>Builder</h4>
+                <h4><a href="./Builder/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Builder/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Builder/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./ChainOfResponsibility/uml/uml.png" alt="Adapter">
+                <h4>ChainOfResponsibility</h4>
+                <h4><a href="./ChainOfResponsibility/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./ChainOfResponsibility/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./ChainOfResponsibility/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Command/uml/uml.png" alt="Adapter">
+                <h4>Command</h4>
+                <h4><a href="./Command/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Command/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Command/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Composite/uml/uml.png" alt="Adapter">
+                <h4>Composite</h4>
+                <h4><a href="./Composite/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Composite/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Composite/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="#" alt="Adapter">
+                <h4>Composition</h4>
+                <h4><a href="#" target="_blank">NO</a></h4>
+                <h4><a href="#" target="_blank">NO</a></h4>
+                <h4><a href="#" target="_blank">NO</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./DataMapper/uml/uml.png" alt="Adapter">
+                <h4>DataMapper</h4>
+                <h4><a href="./DataMapper/StorageAdapter.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./DataMapper/StorageAdapter.php" target="_blank">Real World</a></h4>
+                <h4><a href="./DataMapper/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>SI</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Deorator/uml/uml.png" alt="Adapter">
+                <h4>Deorator</h4>
+                <h4><a href="./Deorator/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Deorator/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Deorator/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./DependencyInjection/uml/uml.png" alt="Adapter">
+                <h4>DependencyInjection</h4>
+                <h4><a href="./DependencyInjection/DatabaseConfiguration" target="_blank">Conceptual</a></h4>
+                <h4><a href="./DependencyInjection/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./DependencyInjection/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./EAV/uml/uml.png" alt="Adapter">
+                <h4>EAV</h4>
+                <h4><a href="./EAV/Attribute.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./EAV/Attribute.php" target="_blank">Real World</a></h4>
+                <h4><a href="./EAV/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Facade/uml/uml.png" alt="Adapter">
+                <h4>Facade</h4>
+                <h4><a href="./Facade/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Facade/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Facade/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./FactoryMethod/uml/uml.png" alt="Adapter">
+                <h4>FactoryMethod</h4>
+                <h4><a href="./FactoryMethod/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./FactoryMethod/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./FactoryMethod/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Facade/uml/uml.png" alt="Adapter">
+                <h4>FluentInterface</h4>
+                <h4><a href="./FluentInterface/Tests/FluentInterfaceTest.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./FluentInterface/Tests/FluentInterfaceTest.php" target="_blank">Real World</a></h4>
+                <h4><a href="./FluentInterface/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Flyweight/uml/uml.png" alt="Adapter">
+                <h4>Flyweight</h4>
+                <h4><a href="./Flyweight/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Flyweight/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Flyweight/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Interpreter/uml/uml.png" alt="Adapter">
+                <h4>Interpreter</h4>
+                <h4><a href="./Interpreter/RealWorld/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Interpreter/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Interpreter/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Iterator/uml/uml.png" alt="Adapter">
+                <h4>Iterator</h4>
+                <h4><a href="./Iterator/RealWorld/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Iterator/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Iterator/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Mediator/uml/uml.png" alt="Adapter">
+                <h4>Mediator</h4>
+                <h4><a href="./Mediator/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Mediator/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Mediator/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Memento/uml/uml.png" alt="Adapter">
+                <h4>Memento</h4>
+                <h4><a href="./Memento/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Memento/Conceptual/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Memento/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./NullObject/uml/uml.png" alt="Adapter">
+                <h4>NullObject</h4>
+                <h4><a href="./NullObject/Service.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./NullObject/Service.php" target="_blank">Real World</a></h4>
+                <h4><a href="./NullObject/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Observer/uml/uml.png" alt="Adapter">
+                <h4>Observer</h4>
+                <h4><a href="./Observer/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Observer/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Observer/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Observer/uml/uml.png" alt="Adapter">
+                <h4>Pool</h4>
+                <h4><a href="./Pool/WorkerPool.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Pool/WorkerPool.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Pool/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Prototype/uml/uml.png" alt="Adapter">
+                <h4>Prototype</h4>
+                <h4><a href="./Prototype/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Prototype/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Prototype/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Proxy/uml/uml.png" alt="Adapter">
+                <h4>Proxy</h4>
+                <h4><a href="./Proxy/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Proxy/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Proxy/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Registry/uml/uml.png" alt="Adapter">
+                <h4>Registry</h4>
+                <h4><a href="./Registry/Service.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Registry/Service.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Registry/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Repository/uml/uml.png" alt="Adapter">
+                <h4>Repository</h4>
+                <h4><a href="./Repository/Test/PostRepositoryTest.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Repository/Test/PostRepositoryTest.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Repository/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./ServiceLocator/uml/uml.png" alt="Adapter">
+                <h4>ServiceLocator</h4>
+                <h4><a href="./ServiceLocator/Tests/ServiceLocatorTest.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./ServiceLocator/Tests/ServiceLocatorTest.php" target="_blank">Real World</a></h4>
+                <h4><a href="./ServiceLocator/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./SimpleFactory/uml/uml.png" alt="Adapter">
+                <h4>SimpleFactory</h4>
+                <h4><a href="./SimpleFactory/Tests/SimpleFactoryTest.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./SimpleFactory/Tests/SimpleFactoryTest.php" target="_blank">Real World</a></h4>
+                <h4><a href="./SimpleFactory/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Singleton/uml/uml.png" alt="Adapter">
+                <h4>Singleton</h4>
+                <h4><a href="./Singleton/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Singleton/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Singleton/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Specification/uml/uml.png" alt="Adapter">
+                <h4>Specification</h4>
+                <h4><a href="./Specification/Tests/SpecificationTest.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Specification/Tests/SpecificationTest.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Specification/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./State/uml/uml.png" alt="Adapter">
+                <h4>State</h4>
+                <h4><a href="./State/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./State/Conceptual/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./State/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./StaticFactory/uml/uml.png" alt="Adapter">
+                <h4>StaticFactory</h4>
+                <h4><a href="./StaticFactory/Tests/StaticFactoryTest.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./StaticFactory/Tests/StaticFactoryTest.php" target="_blank">Real World</a></h4>
+                <h4><a href="./StaticFactory/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Strategy/uml/uml.png" alt="Adapter">
+                <h4>Strategy</h4>
+                <h4><a href="./Strategy/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Strategy/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Strategy/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./TemplateMethod/uml/uml.png" alt="Adapter">
+                <h4>TemplateMethod</h4>
+                <h4><a href="./TemplateMethod/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./TemplateMethod/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./TemplateMethod/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+            <div class="table-body">
+                <img src="./Visitor/uml/uml.png" alt="Adapter">
+                <h4>Visitor</h4>
+                <h4><a href="./Visitor/Conceptual/index.php" target="_blank">Conceptual</a></h4>
+                <h4><a href="./Visitor/RealWorld/index.php" target="_blank">Real World</a></h4>
+                <h4><a href="./Visitor/uml/uml.png" target="_blank">UML</a></h4>
+                <h4>NO</h4>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
